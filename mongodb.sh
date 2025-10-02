909d603268f8b4866418aa2561dafdfda9acc40c
Kishore7711 <kishorebalusu7@gmail.com>
2025-10-03 00:03:18 +0530

    commits done

---
+++ mongodb.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"  #### N for Normal Color
+
+LOGS_FOLDER="/var/log/Shell-Roboshop"
+SCRIPT_NAME=$(echo "mongodb.sh" | cut -d "." -f1)
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"  ### /var/log/Shell-Roboshop/mongodb.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script Exectution Started at : $(date)" | tee -a $LOGS_FILE
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:: $N Please run this script with $B ROOT Prilivages $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ] ; then 
+        echo -e "$2 ....  $R Failed $N" | tee -a $LOGS_FILE
+    else
+        echo -e "$2 ....  $G Successful $N" | tee -a $LOGS_FILE
+    fi
+         }
+
+        
+  cp mongo.repo /etc/yum.repos.d/mongo.repo
+  VALIDATE $? "Adding Mongodb Repo"
+
+  dnf install mongodb-org -y &>>$LOGS_FILE
+  VALIDATE $? "Installing Mongodb"
+
+  systemctl enable mongod &>>$LOGS_FILE
+  VALIDATE $? "Enabling Mongodb"
+
+  systemctl start mongod &>>$LOGS_FILE
+  VALIDATE $? "Starting Mongodb"
+
+
+
