772b6c3771157a67dfefbfaa775aa83d88941fdf
Kishore7711 <kishorebalusu7@gmail.com>
2025-10-04 22:29:15 +0530

    commits

--- mysql.sh
+++ mysql.sh
@@ -32,24 +32,10 @@ VALIDATE(){
     fi
          }
 
-    dnf module disable redis -y &>>$LOGS_FILE
-    VALIDATE $? "Diable Default redis"
+    
+dnf install mysql-server -y
 
-    dnf module enable redis:7 -y &>>$LOGS_FILE
-    VALIDATE $? "Enable redis 7"
+systemctl enable mysqld
 
-    dnf install redis -y &>>$LOGS_FILE
-    VALIDATE $? "Installing redis 7"
+systemctl start mysqld
 
-    sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 'protected-mode/ c protected-mode no' /etc/redis/redis.conf  ### ( sed -i means pariminent changes)
-    VALIDATE $? "Allow Remote Connections to Redis"
-
-    systemctl enable redis &>>$LOGS_FILE
-    VALIDATE $? "Enable redis"
-
-    systemctl start redis &>>$LOGS_FILE
-    VALIDATE $? "Start redis"
-
-    END_TIME=$(date +%s)
-    TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-    echo  -e "Script Executed in $Y $TOTAL_TIME seconds $N"
