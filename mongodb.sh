8a3637a263e8d58cfbd6fbd9bc1e687970107827
Kishore7711 <kishorebalusu7@gmail.com>
2025-10-04 15:28:31 +0530

    commits

--- mongodb.sh
+++ mongodb.sh
@@ -49,3 +49,12 @@ VALIDATE(){
 
   systemctl restart mongod &>>$LOGS_FILE
   VALIDATE $? "Restarting Mongodb"
+
+  systemctl ps -ef | grep mongo
+  VALIDATE $? "Mongodb service running"
+
+  systemctl status mongod &>>$LOGS_FILE
+  VALIDATE $? "Active the mongodb service"
+
+  systemctl netstat -nltp
+  VALIDATE $? "Mongodb port is open"
